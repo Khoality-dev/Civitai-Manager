cd6263bc5743261821614b9d96b5a987e1b0ce1b
Khoa <magicmonster17@gmail.com>
2024-05-18 15:25:57 -0700

    remove my context

--- app_configs.py
+++ app_configs.py
@@ -1,6 +1,7 @@
 import os
 
 CIVITAI_API_KEY = os.environ.get('CIVITAI_API_KEY')
-MODELS_DIRECTORY = "E:/Application/stable-diffusion-windows/stable-diffusion-webui/models" # if not specify, create a new models folder at current path and save all the files there
+MODELS_DIRECTORY = None # if not specify, create a new models folder at current path and save all the files there
 IMAGES_DIRECTORY = None # if not specify, civitai manager will save images to model folder
 ALBUM_DIRECTORY = None
+BACKUP_MODELS_DIRECTORY = None
